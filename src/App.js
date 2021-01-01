1d5d20919b048e057b540d8945c83f4e0cc343b0
StoryOff <sotic.roma@gmail.com>
2021-01-02 05:42:55 +0800

    no message

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import TodoList from './Todo/TodoList'
+import React, {useEffect} from 'react'
+import Context from './context'
+import Loader from './Loader'
+import Modal from './Modal/Modal'
+
+const AddTodo = React.lazy(() => import('./Todo/AddTodo'))
 
 function App() {
+  const [todos, setTodos] = React.useState([])
+  const [loading, setLoading] = React.useState(true)
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos?_limit=5')
+      .then(response => response.json())
+      .then(todos => {
+          setTodos(todos)
+          setLoading(false)
+      })
+  }, [])
+
+  function toggleTodo(id) {
+    setTodos( 
+      todos.map(todo => {
+        if (todo.id === id) {
+         todo.completed = !todo.completed
+        }
+        return todo
+      })
+    )
+  }
+
+  function removeTodo(id){
+    setTodos(todos.filter(todo => todo.id !== id))
+  }
+
+  function addTodo(title) {
+    setTodos(todos.concat([{
+      title,
+      id: Date.now(),
+      completed: false
+    }]))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Context.Provider value={{ removeTodo }}>
+      <div className = 'wrapper'>
+        <h1>Todo List</h1>
+        <Modal />
+        <React.Suspense fallback={<p>Loading...</p>}>
+          <AddTodo onCreate={addTodo} />
+        </React.Suspense>
+
+        {loading && <Loader/>}
+        {todos.length ? <TodoList todos={ todos } onToggle={toggleTodo}/> : loading? null : <p>Todo List Empty!</p>}
+      </div>
+    </Context.Provider>
   );
 }
 
